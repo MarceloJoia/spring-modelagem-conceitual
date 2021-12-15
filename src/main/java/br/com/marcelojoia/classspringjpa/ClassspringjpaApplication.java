8c386ae3106ed4bd2d7f1144f2a39fc81858e73d
MarceloJoia <sitejoia@hotmail.com>
2021-12-15 13:19:48 -0300

    first commit

---
+++ src/main/java/br/com/marcelojoia/classspringjpa/ClassspringjpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelojoia.classspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassspringjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassspringjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcelojoia/classspringjpa/ClassspringjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelojoia.classspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassspringjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
